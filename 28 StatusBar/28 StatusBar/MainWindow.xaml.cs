3dd646a8db314cafea95cc47ec18a8a29e460cdf
Mcniven01 <146711457+JMcniven1@users.noreply.github.com>
2025-11-23 21:12:41 +0100

    tutorials menu and barstatus
    tutorials 27 and 28

---
+++ 28 StatusBar/28 StatusBar/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _28_StatusBar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void txtDatos_KeyDown(object sender, KeyEventArgs e)
+        {
+            //El +1 es porque usamos KeyDown
+            int cantidad = txtDatos.Text.Length + 1;
+
+            String[] palabras = txtDatos.Text.Split(new char[] { ' ' });
+            int cantPalabras = palabras.Length;
+
+            txtInformation.Text = "Se tienen " + cantidad.ToString() + "caracteres";
+            txtPalabras.Text = "con" + cantPalabras.ToString() + " palabras";
+        } 
+    }
+}
