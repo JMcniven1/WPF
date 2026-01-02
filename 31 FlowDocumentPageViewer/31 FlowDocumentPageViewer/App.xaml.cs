8f6a84ac0773b9525a4e2ce95886a0bb714b447c
Mcniven01 <146711457+JMcniven1@users.noreply.github.com>
2026-01-02 17:32:23 +0100

    Changes new Tutorials
    New documents and tutorials

---
+++ 31 FlowDocumentPageViewer/31 FlowDocumentPageViewer/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _31_FlowDocumentPageViewer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
