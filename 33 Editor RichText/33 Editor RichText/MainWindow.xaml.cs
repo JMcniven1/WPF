99d706a7b09962cb155963b5d86b5c0d94e3f05f
Mcniven01 <146711457+JMcniven1@users.noreply.github.com>
2026-03-10 21:18:15 +0100

    add changes

---
+++ 33 Editor RichText/33 Editor RichText/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _33_Editor_RichText
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Rellenamos el comboBox de la tipografia
+            cmbTipografia.ItemsSource = Fonts.SystemFontFamilies.OrderBy(f => f.Source);
+            //Rellenamos el comboBox de los tamanos
+            cmbTamano.ItemsSource = new List<double>() { 8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 36, 48, 72 };
+
+        }
+
+        private void rbtTexto_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void cmbTipografia_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cmbTipografia.SelectedItem != null)
+                rtbTexto.Selection.ApplyPropertyValue(Inline.FontFamilyProperty, cmbTipografia.SelectedItem);
+        }
+        private void cmbTamano_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            rtbTexto.Selection.ApplyPropertyValue(Inline.FontSizeProperty, cmbTamano.Text);
+        }
+
+
+    }
+}
---
+++ 34 Border/34 Border/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _34_Border
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
